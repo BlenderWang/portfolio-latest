d096ac09fc09312630e32d7a610875af201eb410
Shirley Wang <shirleywang527@gmail.com>
2019-12-29 22:11:56 +0100

    change tabindex to tabIndex

--- src/components/Hamburger.js
+++ src/components/Hamburger.js
@@ -4,14 +4,14 @@ export default props => {
   return (
     <div
       role="button"
-      tabindex="0"
+      tabIndex="0"
       className="menu-btn"
       onClick={props.handleClick}
       onKeyDown={props.handleClick}
     >
       <span
         role="button"
-        tabindex="0"
+        tabIndex="0"
         className={props.hamburgerClass}
         onClick={props.handleClick}
         onKeyDown={props.handleClick}
