a117edc899af528ed62ac08d8d754495c7be6582
Shirley Wang <shirleywang527@gmail.com>
2020-01-02 20:23:29 +0100

    contact page

---
+++ src/components/Form.js
@@ -0,0 +1,107 @@
+import React from "react"
+import { navigate } from "gatsby-link"
+
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&")
+}
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error))
+  }
+
+  render() {
+    return (
+      <div className="contact-form">
+        <form
+          name="contact"
+          method="post"
+          action="/success"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
+        >
+          <input type="hidden" name="form-name" value="contact" />
+          <div hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="field">
+            {/* <label className="label" htmlFor={"name"}></label> */}
+            <div className="control">
+              <input
+                className="input"
+                type={"text"}
+                name={"name"}
+                onChange={this.handleChange}
+                id={"name"}
+                placeholder="Your name"
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="field">
+            {/* <label className="label" htmlFor={"email"}></label> */}
+            <div className="control">
+              <input
+                className="input"
+                type={"email"}
+                name={"email"}
+                onChange={this.handleChange}
+                id={"email"}
+                placeholder="Email"
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="field">
+            {/* <label className="label" htmlFor={"message"}></label> */}
+            <div className="control">
+              <textarea
+                className="textarea"
+                name={"message"}
+                rows="7"
+                onChange={this.handleChange}
+                id={"message"}
+                placeholder="Message"
+                required={true}
+              />
+            </div>
+          </div>
+          <div className="field btns">
+            <input type="submit" value="Send" className="btn btn--primary" />
+            <input
+              type="reset"
+              value="Clear"
+              className="btn btn--danger"
+            ></input>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout"
 import Menu from "../components/Menu"
 import Footer from "../components/Footer"
 import TitleText from "../components/TitleText"
+import Paragraph from "../components/Paragraph"
+import Form from "../components/Form"
 import SEO from "../components/seo"
 
 const contact = () => {
@@ -11,6 +13,18 @@ const contact = () => {
       <SEO title="Page contact" />
       <Menu />
       <TitleText titleText={"contact"} />
+      <div className="content content-contact-page">
+        <div className="contact-texts">
+          <Paragraph paragraph={`Send an `} />
+          <Paragraph span={`email`} />
+          <Paragraph paragraph={`to me or fill out the `} />
+          <Paragraph span={`contact form`} />
+          <Paragraph
+            paragraph={`below. I'd love to hear from you at any time so don't be a stranger!`}
+          />
+        </div>
+        <Form />
+      </div>
       <Footer />
     </Layout>
   )
