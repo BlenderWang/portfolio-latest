87fd8a95fd02de20a0304bbafcb56ee39fe1d88d
Shirley Wang <shirleywang527@gmail.com>
2020-01-10 07:47:48 +0100

    svg anim

---
+++ src/components/Svg.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "../styles/svg.scss"
+
+const Svg = () => {
+  return (
+    <div>
+      <svg
+        id="logo"
+        width="219"
+        height="109"
+        viewBox="0 0 219 109"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          className="path path-left"
+          d="M146.101 3.33039L145.781 2.25999H144.664H115H112.842L113.594 4.28277L151.61 106.523L151.973 107.5H153.016H176.344H177.391L177.752 106.518L215.336 4.27754L216.078 2.25999H213.928H184.408H183.286L182.969 3.33607L164.67 65.4478L146.101 3.33039Z"
+          stroke="black"
+          strokeWidth="3"
+        />
+        <path
+          className="path path-right"
+          d="M36.1012 3.33039L35.7812 2.25999H34.664H5H2.84192L3.59405 4.28277L41.6101 106.523L41.9734 107.5H43.016H66.344H67.3907L67.7519 106.518L105.336 4.27754L106.078 2.25999H103.928H74.408H73.2862L72.9692 3.33607L54.6696 65.4478L36.1012 3.33039Z"
+          stroke="black"
+          strokeWidth="3"
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default Svg
