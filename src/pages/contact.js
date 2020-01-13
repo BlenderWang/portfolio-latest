57a982f75f51c2c8890b13dc78e96820853d06ba
Shirley Wang <shirleywang527@gmail.com>
2020-01-13 21:16:27 +0100

    form and contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,25 +7,52 @@ import Paragraph from "../components/Paragraph"
 import Form from "../components/Form"
 import SEO from "../components/seo"
 import { Link } from "gatsby"
+import { Spring } from "react-spring/renderprops"
 
 const contact = () => {
   return (
     <Layout>
       <SEO title="Page contact" />
       <Menu />
-      <TitleText titleText={"contact"} />
-      <div className="content content-contact-page">
-        <div className="contact-texts">
-          <Paragraph paragraph={`Send an `} />
-          <Paragraph span={`email `} />
-          <Paragraph paragraph={`to me or fill out the `} />
-          <Paragraph span={` contact form `} />
-          <Paragraph
-            paragraph={`below. I'd love to hear from you at any time so don't be a stranger!`}
-          />
-        </div>
-        <Form />
-      </div>
+      <Spring
+        from={{ opacity: 0, transform: `translate3d(300px, 0, 0)` }}
+        to={{ opacity: 1, transform: `translate3d(0, 0, 0)` }}
+        config={{ duration: 1250 }}
+      >
+        {props => (
+          <div style={props}>
+            <TitleText titleText={"contact"} />
+          </div>
+        )}
+      </Spring>
+      <Spring
+        from={{ opacity: 0, transform: `translate3d(0, -50px, 0)` }}
+        to={{ opacity: 1, transform: `translate3d(0, 0, 0)` }}
+        config={{ delay: 750, duration: 1000 }}
+      >
+        {props => (
+          <div style={props} className="content content-contact-page">
+            <Spring
+              from={{ opacity: 0, transform: `translate3d(0, 50px, 0)` }}
+              to={{ opacity: 1, transform: `translate3d(0, 0, 0)` }}
+              config={{ delay: 1200, duration: 1000 }}
+            >
+              {props => (
+                <div style={props} className="contact-texts">
+                  <Paragraph paragraph={`Send an `} />
+                  <Paragraph span={`email `} />
+                  <Paragraph paragraph={`to me or fill out the `} />
+                  <Paragraph span={` contact form `} />
+                  <Paragraph
+                    paragraph={`below. I'd love to hear from you at any time so don't be a stranger!`}
+                  />
+                </div>
+              )}
+            </Spring>
+            <Form />
+          </div>
+        )}
+      </Spring>
       <Link to="/">
         <span className="arrow">&#8594;</span>
       </Link>
