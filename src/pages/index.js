e2ce3fa7327f30ea981d1331538614e4d5ca2b51
Shirley Wang <shirleywang527@gmail.com>
2020-01-13 20:28:42 +0100

    try add spring back

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,84 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Spring } from "react-spring/renderprops"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Menu from "../components/Menu"
 import TitleText from "../components/TitleText"
 import Title from "../components/Title"
 import Paragraph from "../components/Paragraph"
+import Svg from "../components/Svg"
 import Footer from "../components/Footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Menu />
-    <TitleText titleText={"hey"} />
+    <Svg />
+    <Spring
+      from={{ opacity: 0 }}
+      to={{ opacity: 1 }}
+      config={{ delay: 3500, duration: 1000 }}
+    >
+      {opacity => (
+        <div style={opacity}>
+          <Menu />
+        </div>
+      )}
+    </Spring>
+    <Spring
+      from={{ transform: `translateX(100%)`, opacity: 0 }}
+      to={{ transform: `translateX(0%)`, opacity: 1 }}
+      config={{ delay: 3500, duration: 2000 }}
+    >
+      {props => (
+        <div style={props}>
+          <TitleText titleText={"hey"} />
+        </div>
+      )}
+    </Spring>
     <div className="all-texts">
-      <Title title={`I'm `} titleSpan={`Shirley Wang`} />
-      <Paragraph span={`Front-End `} paragraph={`Developer, base in `} />
-      <Paragraph span={`Stockholm`} paragraph={` Sweden`} />
+      <Spring
+        from={{ transform: `translateY(-50px)`, opacity: 0 }}
+        to={{ transform: `translateY(0)`, opacity: 1 }}
+        config={{ delay: 6200, duration: 500 }}
+      >
+        {props => (
+          <div style={props}>
+            <Title title={`I'm `} titleSpan={`Shirley Wang`} />
+          </div>
+        )}
+      </Spring>
+      <Spring
+        from={{ transform: `translateY(50px)`, opacity: 0 }}
+        to={{ transform: `translateY(0)`, opacity: 1 }}
+        config={{ delay: 6500, duration: 500 }}
+      >
+        {props => (
+          <div style={props}>
+            <div style={props}>
+              <Paragraph
+                span={`Front-End `}
+                paragraph={`Developer, base in `}
+              />
+              <Paragraph span={`Stockholm`} paragraph={` Sweden`} />
+            </div>
+          </div>
+        )}
+      </Spring>
     </div>
-    <Link to="/about">
-      <span className="arrow">&#8594;</span>
-    </Link>
+    <Spring
+      from={{ opacity: 0 }}
+      to={{ opacity: 1 }}
+      config={{ delay: 3500, duration: 1000 }}
+    >
+      {opacity => (
+        <div style={opacity}>
+          <Link to="/about">
+            <span className="arrow">&#8594;</span>
+          </Link>
+        </div>
+      )}
+    </Spring>
     <Footer />
   </Layout>
 )
